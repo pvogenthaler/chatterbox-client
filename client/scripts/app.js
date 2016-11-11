0185e257d55868be2c79aaaaa1fd795220185e8d
pvogenthaler <paige.vogenthaler@gmail.com>
2016-11-11 15:28:19 -0800

    skeleton code for app

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,45 @@
-// YOUR CODE HERE:
+var app = {};
+
+app.init = function() {
+
+};
+
+app.send = function() {
+
+};
+
+app.fetch = function() {
+
+};
+
+//clears from appropriate div element
+app.clearMessages = function() {
+
+};
+
+
+
+
+
+
+// console.log('hello');
+// var message = {
+//   username: 'shawndrost',
+//   text: 'trololo',
+//   roomname: '4chan'
+// };
+
+// $.ajax({
+//   // This is the url you should use to communicate with the parse API server.
+//   url: 'https://api.parse.com/1/classes/messages',
+//   type: 'POST',
+//   data: JSON.stringify(message),
+//   contentType: 'application/json',
+//   success: function (data) {
+//     console.log('chatterbox: Message sent');
+//   },
+//   error: function (data) {
+//     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//     console.error('chatterbox: Failed to send message', data);
+//   }
+// });
